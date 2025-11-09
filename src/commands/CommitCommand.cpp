bd2cd173e067a61f57f22b363f082ad89cc279f7
mahmoodana1 <arabg1999@gmail.com>
2025-11-09 14:36:04 +0200

    reached the message creation in CommitCommand

--- src/commands/CommitCommand.cpp
+++ src/commands/CommitCommand.cpp
@@ -1,10 +1,23 @@
 #include "../../include/commands/CommitCommand.h"
+#include "../../include/utils/IdGenerator.h"
 #include <iostream>
+#include <memory>
+#include <vector>
+
+std::string getCommitMessage(const std::vector<std::string> &args) {
+    std::ostringstream messageStream;
+
+    for (int i = 2; i < args.size(); i++) {
+        messageStream << args[i] << ' ';
+    }
+
+    return messageStream.str();
+}
 
 std::string CommitCommand::getName() { return "commit"; }
 
 bool CommitCommand::checkArgs(const std::vector<std::string> &args) {
-    if (args.size() != 3 || args[2] != "-m")
+    if (args.size() < 3 || args[1] != "-m")
         return false;
 
     return true;
@@ -19,4 +32,17 @@ void CommitCommand::execute(const std::vector<std::string> &args) {
     if (!Utils::exists(".minigit") || !Utils::exists(".minigit/index")) {
         std::cout << "Repository not initialized correctly\n";
     }
+
+    std::string commitId = GeneratorUtil::generateCommitId();
+    std::string commitMessage = getCommitMessage(args);
 }
+
+namespace {
+struct CommitCommandRegisterar {
+    CommitCommandRegisterar() {
+        CommandRegistry::getInstance().registerCommand(
+            "commit", std::make_unique<CommitCommand>());
+    }
+};
+static CommitCommandRegisterar registerar;
+} // namespace
