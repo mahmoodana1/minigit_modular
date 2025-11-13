413b5bc8342592bc114ae72e618f94314b113b10
mahmoodana1 <arabg1999@gmail.com>
2025-11-13 13:43:22 +0200

    started the LogCommand

---
+++ include/commands/LogCommand.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Command.h"
+
+class LogCommand : public Command {
+    void execute(const std::vector<std::string> &args) override;
+    bool checkArgs(const std::vector<std::string> &args) override;
+    void description() override;
+    std::string getName() override;
+};
